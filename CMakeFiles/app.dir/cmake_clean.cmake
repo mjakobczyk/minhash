64e38d979e238e7296c9e209e0d5a9d7d6aeb673
mjakobczyk <m.jakob@wp.pl>
2018-10-07 19:33:54 +0200

    Change from Makefile to CMakeLists, delete local boost library, refactor directories structure
    Former-commit-id: 9434afed2ab1b6c14d42a47e990a5cdad089ad35

---
+++ CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/src/extensiondetector.cpp.obj"
+  "CMakeFiles/app.dir/src/filemanager.cpp.obj"
+  "CMakeFiles/app.dir/src/main.cpp.obj"
+  "CMakeFiles/app.dir/src/osdetector.cpp.obj"
+  "app.pdb"
+  "app.exe"
+  "app.exe.manifest"
+  "libapp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include "osdetector.h"
 #include "extensiondetector.h"
+#include <iostream>
 
 int main()
 {
+	std::cout << "Hello world!" << std::endl;
+
 	OSDetector osDetector;
 	osDetector.checkOS();
 
 	ExtensionDetector exDetector;
 	exDetector.checkAvailableExtensions();
+
+	char stopper;
+	std::cin >> stopper;
 	
 	return 0;
 }
