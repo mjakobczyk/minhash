a00bf8843bb6389453d60594f3819947d87ce36f
i354853 <michal.jakobczyk@sap.com>
2018-10-10 11:08:02 +0200

    Update SIMD PoC, add exec to .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,6 @@ int main()
 	ExtensionDetector exDetector;
 	exDetector.checkAvailableExtensions();
 
-	__m128i v1 = _mm_set_epi32(1, 1, 1, 1);
-	__m128i v2 = _mm_set1_epi32(2);
-	__m128i v3 = _mm_add_epi32(v1, v2);
-
-	int tab[4];
-
 	char stopper;
 	std::cin >> stopper;
 	
