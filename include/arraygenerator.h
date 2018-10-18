b5e71a59c827177179ebba2679a5d44d35850e3f
mjakobczyk <m.jakob@wp.pl>
2018-10-18 16:29:48 +0200

    Create array generator

---
+++ include/arraygenerator.h
@@ -0,0 +1,11 @@
+#ifndef ARRAYGENERATOR_H_
+#define ARRAYGENERATOR_H_
+#include <iostream>
+
+class ArrayGenerator
+{
+public:
+    uint64_t * generateUint64Array(int amount);
+};
+
+#endif
---
+++ src/arraygenerator.cpp
@@ -0,0 +1,6 @@
+#include "arraygenerator.h"
+
+uint64_t * ArrayGenerator::generateUint64Array(int amount)
+{
+    // https://gist.github.com/PhDP/5289449
+}
