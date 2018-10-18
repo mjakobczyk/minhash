4b95e63dd0119295514faee3a2ec1120d6310f28
MJ <Majki@MacBook-Pro-Beata.local>
2018-10-18 22:01:16 +0200

    Add random uint_64 generator, add generator to CMakeLists

--- include/arraygenerator.h
+++ include/arraygenerator.h
@@ -1,11 +1,21 @@
 #ifndef ARRAYGENERATOR_H_
 #define ARRAYGENERATOR_H_
 #include <iostream>
+#include <random>
+#include <ctime>
 
 class ArrayGenerator
 {
+    unsigned int seed;
+    std::mt19937_64 engine;
+    std::uniform_int_distribution<uint64_t> distribution;
+
 public:
-    uint64_t * generateUint64Array(int amount);
+    ArrayGenerator();
+    uint64_t * generateRandomUint64Array(int amount);
+
+private:
+    uint64_t generateRandomUint64();
 };
 
 #endif
--- src/arraygenerator.cpp
+++ src/arraygenerator.cpp
@@ -1,6 +1,22 @@
 #include "arraygenerator.h"
 
-uint64_t * ArrayGenerator::generateUint64Array(int amount)
+ArrayGenerator::ArrayGenerator() : seed(time(0)), engine(seed), distribution(0, 33333333)
+{
+    // this->seed = time(0);
+    // this->engine(this->seed);
+    // this->distribution(0, UINT64_MAX);
+}
+
+uint64_t * ArrayGenerator::generateRandomUint64Array(int amount)
 {
     // https://gist.github.com/PhDP/5289449
+    uint64_t * array = new uint64_t[2];
+    return array;
+}
+
+uint64_t ArrayGenerator::generateRandomUint64()
+{
+    std::cout << "Generating... ";
+    std::cout << this->distribution(this->engine);
+    return 1;
 }
